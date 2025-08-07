9ee4661e90564fef80925ea37deea6ade0770900
Humberto Correia <correia.humberto@gmail.com>
2025-08-07 23:53:21 +0100

    feat(js-adonisjs): Dockerfile and routes

--- js-adonisjs/start/routes.ts
+++ js-adonisjs/start/routes.ts
@@ -7,10 +7,26 @@
 |
 */
 
+import { HttpContext } from '@adonisjs/core/http'
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
   return {
-    hello: 'world',
+    result: 'Ok',
+  }
+})
+router.get('/db/', async ({ response }: HttpContext) => {
+  return response.notImplemented({
+    result: 'Not implemented!',
+  })
+})
+router.get('/chaos/', async ({ response }: HttpContext) => {
+  return response.notImplemented({
+    result: 'Not implemented!',
+  })
+})
+router.get('/health/', async () => {
+  return {
+    result: 'Ok',
   }
 })
