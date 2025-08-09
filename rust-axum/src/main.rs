5d68a40c1fadd65445a1895a94b413f6248c877d
Humberto Correia <correia.humberto@gmail.com>
2025-08-09 14:35:58 +0100

    feat(rust-axum): init

---
+++ rust-axum/src/main.rs
@@ -0,0 +1,52 @@
+use axum::{Router, http::StatusCode, response, routing::get};
+
+use serde_json::json;
+
+#[tokio::main()]
+async fn main() {
+    let app = Router::new()
+        .route(
+            "/",
+            get(|| async {
+                return response::Json(json!({
+                    "result": "Ok"
+                }));
+            }),
+        )
+        .route(
+            "/db/",
+            get(|| async {
+                return (
+                    StatusCode::NOT_IMPLEMENTED,
+                    response::Json(json!({
+                        "result": "Not implemented!"
+                    })),
+                );
+            }),
+        )
+        .route(
+            "/chaos/",
+            get(|| async {
+                return (
+                    StatusCode::NOT_IMPLEMENTED,
+                    response::Json(json!({
+                        "result": "Not implemented!"
+                    })),
+                );
+            }),
+        )
+        .route(
+            "/health/",
+            get(|| async {
+                return response::Json(json!({
+                    "result": "Ok"
+                }));
+            }),
+        );
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
