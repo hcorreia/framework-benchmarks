3c3d71893cd90572e6f8999f2c77a6ac4beaa71c
Humberto Correia <correia.humberto@gmail.com>
2025-08-10 00:18:04 +0100

    feat(rust-axum): Chaos server, show hostname

--- rust-axum/src/main.rs
+++ rust-axum/src/main.rs
@@ -1,48 +1,82 @@
-use axum::{Router, http::StatusCode, response, routing::get};
+use std::{fs, time};
 
+use rand::random;
+
+use axum::{Router, extract::State, http::StatusCode, response, routing::get};
 use serde_json::json;
+
+#[derive(Clone)]
+struct AppState {
+    hostname: String,
+}
 
 #[tokio::main()]
 async fn main() {
+    let hostname = fs::read_to_string("/etc/hostname")
+        .unwrap()
+        .trim()
+        .to_string();
+
+    let app_state = AppState { hostname: hostname };
+
     let app = Router::new()
         .route(
             "/",
-            get(|| async {
+            get(|State(state): State<AppState>| async move {
                 return response::Json(json!({
-                    "result": "Ok"
+                    "result": "Ok",
+                    "hostname": state.hostname,
                 }));
             }),
         )
         .route(
             "/db/",
-            get(|| async {
+            get(|State(state): State<AppState>| async move {
                 return (
                     StatusCode::NOT_IMPLEMENTED,
                     response::Json(json!({
-                        "result": "Not implemented!"
+                        "result": "Not implemented!",
+                        "hostname": state.hostname,
                     })),
                 );
             }),
         )
         .route(
             "/chaos/",
-            get(|| async {
+            get(|State(state): State<AppState>| async move {
                 return (
                     StatusCode::NOT_IMPLEMENTED,
                     response::Json(json!({
-                        "result": "Not implemented!"
+                        "result": "Not implemented!",
+                        "hostname": state.hostname,
                     })),
                 );
             }),
         )
         .route(
             "/health/",
-            get(|| async {
+            get(|State(state): State<AppState>| async move {
                 return response::Json(json!({
-                    "result": "Ok"
+                    "result": "Ok",
+                    "hostname": state.hostname,
                 }));
             }),
-        );
+        )
+        .route(
+            "/chaos-server/",
+            get(|State(state): State<AppState>| async move {
+                let sleep_time = time::Duration::from_millis(random::<u64>() % 100 + 1);
+
+                tokio::time::sleep(sleep_time).await;
+
+                return response::Json(json!({
+                    "result": "Ok",
+                    "hostname": state.hostname,
+                    "sleep_time": sleep_time.as_millis(),
+                }));
+            }),
+        )
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
