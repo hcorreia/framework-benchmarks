55c2cd64d8432d58fe16a019c25bd86525c66fde
Humberto Correia <correia.humberto@gmail.com>
2025-08-10 22:49:14 +0100

    feat(js-adonisjs): add hostname to response

--- js-adonisjs/start/routes.ts
+++ js-adonisjs/start/routes.ts
@@ -7,26 +7,39 @@
 |
 */
 
+import fs from 'node:fs'
+
 import { HttpContext } from '@adonisjs/core/http'
 import Env from '#start/env'
 import router from '@adonisjs/core/services/router'
 
+const hostname = fs.readFileSync('/etc/hostname', 'utf8').trim()
+
 router.get('/', async () => {
   return {
     result: 'Ok',
+    hostname,
   }
 })
 router.get('/db/', async ({ response }: HttpContext) => {
   return response.notImplemented({
     result: 'Not implemented!',
+    hostname,
   })
 })
 router.get('/chaos/', async ({ response }: HttpContext) => {
-  const resp = await fetch(Env.get('CHAOS_ENDPOINT'))
+  let resp
 
-  if (!resp.ok) {
+  try {
+    resp = await fetch(Env.get('CHAOS_ENDPOINT'))
+  } catch (err) {
+    console.error('Chaos service is unavailable.', err)
+  }
+
+  if (!resp?.ok) {
     return response.serviceUnavailable({
       result: 'Chaos service is unavailable.',
+      hostname,
     })
   }
 
@@ -34,11 +47,13 @@ router.get('/chaos/', async ({ response }: HttpContext) => {
 
   return {
     result: 'Ok',
+    hostname,
     sleep_time: data.sleep_time,
   }
 })
 router.get('/health/', async () => {
   return {
     result: 'Ok',
+    hostname,
   }
 })
