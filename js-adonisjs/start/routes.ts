f1b913aaa37df0612f434865dea2f14ac27322df
Humberto Correia <correia.humberto@gmail.com>
2025-08-10 22:30:46 +0100

    feat(js-adonisjs): Chaos endpoint

--- js-adonisjs/start/routes.ts
+++ js-adonisjs/start/routes.ts
@@ -8,6 +8,7 @@
 */
 
 import { HttpContext } from '@adonisjs/core/http'
+import Env from '#start/env'
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
@@ -21,9 +22,20 @@ router.get('/db/', async ({ response }: HttpContext) => {
   })
 })
 router.get('/chaos/', async ({ response }: HttpContext) => {
-  return response.notImplemented({
-    result: 'Not implemented!',
-  })
+  const resp = await fetch(Env.get('CHAOS_ENDPOINT'))
+
+  if (!resp.ok) {
+    return response.serviceUnavailable({
+      result: 'Chaos service is unavailable.',
+    })
+  }
+
+  const data = (await resp.json()) as any
+
+  return {
+    result: 'Ok',
+    sleep_time: data.sleep_time,
+  }
 })
 router.get('/health/', async () => {
   return {
